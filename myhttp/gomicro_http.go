9327124561237cf27ee86c6ef685d30c4b2945cb
kai.zhang_5 <kai.zhang_5@quanshi.com>
2020-05-06 18:24:12 +0800

    增加对 etcd的支持

--- myhttp/gomicro_http.go
+++ myhttp/gomicro_http.go
@@ -7,6 +7,7 @@ import (
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/client/selector"
 	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-micro/registry/etcd"
 	microhttp "github.com/micro/go-plugins/client/http"
 	"github.com/micro/go-plugins/registry/consul"
 	"github.com/micro/go-plugins/wrapper/breaker/hystrix"
@@ -14,18 +15,29 @@ import (
 
 var (
 	ConsulAddr string//consul地址：ip+port
+	EtcdAddr string//consul地址：ip+port
 	DefaultSleepWindow int = 5000//重试时间窗口
 	DefaultTimeOut int = 5000//默认超时时间
 	DefaultVolumeThreshold int = 2//默认最大失败次数
+	RegistryType int = 0//0:etcd ,1:consul
 )
 
 func RequestWithHytrix(serverName, url string, req interface{})map[string]interface{}{
-	consulReg := consul.NewRegistry(
-		registry.Addrs(ConsulAddr),
-	)
+	var reg registry.Registry
+	switch RegistryType {
+	case 0:
+		reg = etcd.NewRegistry(
+			registry.Addrs(EtcdAddr),
+		)
+	case 1:
+		reg = consul.NewRegistry(
+			registry.Addrs(ConsulAddr),
+		)
+	default:
+	}
 
 	microselector := selector.NewSelector(
-		selector.Registry(consulReg),              //传入consul注册
+		selector.Registry(reg),              //传入consul注册
 		selector.SetStrategy(selector.RoundRobin), //指定查询机制
 	)
 	microClient := microhttp.NewClient(
