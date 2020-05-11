c63c694c4caf7691171da5e2a6ac2c7eb54eabb5
kai.zhang_5 <kai.zhang_5@quanshi.com>
2020-05-11 16:04:17 +0800

    add

--- myconfig/myconfig.go
+++ myconfig/myconfig.go
@@ -22,22 +22,20 @@ func (self *IniConfig) GetString(key string) string {
 	return self.GetSectionString("", key)
 }
 
-func (self *IniConfig) GetInt64(key string) int64 {
+func (self *IniConfig) GetInt64(key string) (int64, error) {
 	return self.GetSectionInt64("", key)
 }
 
-func (self *IniConfig) GetInt(key string) int {
+func (self *IniConfig) GetInt(key string) (int, error) {
 	return self.GetSectionInt("", key)
 }
 
-func (self *IniConfig) GetBool(key string) bool {
-	re, _ := self.GetSectionBool("", key)
-	return re
+func (self *IniConfig) GetBool(key string) (bool, error) {
+	return self.GetSectionBool("", key)
 }
 
-func (self *IniConfig) GetFloat64(key string) float64 {
-	re, _ := self.GetSectionFloat64("", key)
-	return re
+func (self *IniConfig) GetFloat64(key string) (float64, error) {
+	return self.GetSectionFloat64("", key)
 }
 
 func (self *IniConfig) GetSectionString(section string, key string) string {
@@ -50,33 +48,31 @@ func (self *IniConfig) GetSectionString(section string, key string) string {
 
 func (self *IniConfig) GetSectionBool(section string, key string) (bool, error) {
 	if self.conf == nil {
-		return false, fmt.Errorf("self.conf is null")
+		return false, fmt.Errorf("config is not init")
 	}
 	s := self.conf.Section(section)
 	return s.Key(key).Bool()
 }
 
-func (self *IniConfig) GetSectionInt64(section string, key string) int64 {
+func (self *IniConfig) GetSectionInt64(section string, key string) (int64, error) {
 	if self.conf == nil {
-		return 0
+		return 0, fmt.Errorf("config is not init")
 	}
 	s := self.conf.Section(section)
-	v, _ := s.Key(key).Int64()
-	return v
+	return s.Key(key).Int64()
 }
 
-func (self *IniConfig) GetSectionInt(section string, key string) int {
+func (self *IniConfig) GetSectionInt(section string, key string) (int, error) {
 	if self.conf == nil {
-		return 0
+		return 0, fmt.Errorf("config is not init")
 	}
 	s := self.conf.Section(section)
-	v, _ := s.Key(key).Int()
-	return v
+	return s.Key(key).Int()
 }
 
 func (self *IniConfig) GetSectionFloat64(section string, key string) (float64, error) {
 	if self.conf == nil {
-		return 0, fmt.Errorf("self.conf is null")
+		return 0, fmt.Errorf("config is not init")
 	}
 	s := self.conf.Section(section)
 	return s.Key(key).Float64()
